ae0f114de79a55d6eb680eb273f9c42edfaf39c4
Esteban Segura Benavides <esteban.1703@gmail.com>
2022-11-10 19:13:21 -0600

    [ADD] Example using Lightning Data Service to send and recieve data to Salesforce database without Apex

---
+++ force-app/main/default/lwc/createContactLDS/createContactLDS.js
@@ -0,0 +1,31 @@
+import { LightningElement,track } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+
+export default class CreateContactLDS extends LightningElement {
+    @track contactName
+    @track contactEmail
+    @track contactPhone
+
+    contactNameChangeHandler(event){
+        this.contactName = event.target.value;
+    }
+
+    contactEmailChangeHandler(event){
+        this.contactEmail = event.target.value;
+    }
+
+    contactPhoneChangeHandler(event){
+        this.contactPhone = event.target.value;
+    }
+
+    createContact(){
+        const fields = {"LastName":this.contactName,"Phone":this.contactPhone,"Email":this.contactEmail};
+        const recordInput = {apiName:"Contact",fields};
+        createRecord(recordInput).then(response=>{
+            console.log('Contact has been created succesfully',response.id);
+        }).catch(error=>{
+            console.log('Error in creating contact, ',error.body.message);
+        });
+
+    }
+}
