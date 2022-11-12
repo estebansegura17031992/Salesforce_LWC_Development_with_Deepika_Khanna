c778ffab5f0aab69406857b9b19320a81dadea19
Esteban Segura Benavides <esteban.1703@gmail.com>
2022-11-12 17:36:51 -0600

    [ADD] Example how fetch data via Ape

---
+++ force-app/main/default/lwc/fetchContactViaApex2/fetchContactViaApex2.js
@@ -0,0 +1,28 @@
+import { LightningElement,track } from 'lwc';
+import getAllContacts from '@salesforce/apex/ContactManager.getContact'
+
+export default class FetchContactViaApex2 extends LightningElement {
+    @track numberOfRecords;
+    @track contacts;
+
+    get responseRecieved(){
+        if(this.contacts)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    numberOfContactChangeHandler(event)
+    {
+        this.numberOfRecords = event.target.value;
+    }
+
+    getContacts(){
+        getAllContacts({numberOfRecords:this.numberOfRecords}).then(response=>{
+            this.contacts = response;
+        }).catch(error=>{
+            console.log('Error in retrieving contact records',error.body.message);
+        });
+    }
+}
